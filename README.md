d23bb36367e22f5d609c000218efd6f9674e9261
Rian Andrea <rian.andrea@gmail.com>
2017-03-25 17:14:36 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+###Flask Hive boilerplate
+
+Flask web framework boilerplate to access [Apache Hive](https://hive.apache.org/) using Cloudera's [Impyla](https://github.com/cloudera/impyla), a python DB API 2.0 client for Impala and Hive (HiveServer2 protocol).
+
+Twitter bootstrap included.
+
+####Prerequisites
+Install impyla
+> pip install impyla
+
+Install thrift_sasl
+> pip install thrift_sasl
+
+Install sasl
+> pip install sasl
+
+Install thrift version 0.9.3
+> pip install thrift==0.9.3
+
+
+#####CONSIDERATIONS 
+Please take note that I don't recommend to use this to query large data sets, as it will be super slow(this is by design, 
+Hive isn't intended for real time usage) depends on the size of the data.
+ 
+My intended usage is only to access summary tables in hive
+ which consist of results of data that are already been processed and ready to show on the dashboard.
+
+  
+ 
+
+   
+   
+     
+   
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from impala.dbapi import connect
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    # CONNECTION TO DATABASE
+    conn = connect(host='your hive db host Ip/hostname', port=10000, auth_mechanism='PLAIN', user='xxx', password='xxx',
+                   database='xxx')
+    c = conn.cursor()
+
+    # QUERIES
+    query_1 = """
+      SELECT *
+      FROM your_table
+    """
+    query_2 = """
+        SELECT *
+        FROM your_table_2 a
+        where to_date(timestamp) = current_date
+        group by x
+    """
+
+    # EXECUTE QUERIES
+
+    # EXE TOTAL
+    c.execute(query_1)
+    result_1 = c.fetchall()
+
+    # EXE TOTAL PER REGION
+    c.execute(query_2)
+    result_2 = c.fetchall()
+    fields = ['field_1', 'field_2', 'field_3']
+    # CONVERT DATA tuple TO dictionary	
+    total_per_region = [dict(zip(fields, d)) for d in reg]
+
+    # RETURN VALUES TO TEMPLATE
+    return render_template('index.html', result1=result_1, result2=result_2)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
